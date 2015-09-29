1ee3080eda0fe8c1b03133a7cdb5bf9a4da53ab8
yuki-shinohara <yuki.shinohara@medpeer.co.jp>
2015-09-30 00:32:29 +0900

    glide対応

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 )
 
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
 func main() {
-	fmt.Print("hello circle ci")
+	goji.Get("/hello/:name", hello)
+	goji.Serve()
 }
